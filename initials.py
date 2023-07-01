bd1f69562e68bc9d00b282e2ea3e7a16527e27cc
Crackingfordopamine <138266156+Crackingfordopamine@users.noreply.github.com>
2023-06-30 22:47:14 -0500

    Add files via upload
    little simple projects for learning python that i made doin the tutorial on Codedex.io

---
+++ initials.py
@@ -0,0 +1,6 @@
+# fun fact: I can easily learn new stuff.
+print("mm     mm      mm     mm")
+print("m m   m m      m m   m m")
+print("m  m m  m      m  m m  m")
+print("m   m   m      m   m   m")
+print("m       m      m       m")
---
+++ letter.py
@@ -0,0 +1,5 @@
+print("1. 06-30-23")
+print("2. Im feeling pretty confident")
+print("3. I like to accomplish from this course is a good understanding of python")
+print("4. You did it!")
+
